c444326042e065bbb8741c518412094c6cdb734e
Nick Linker <nlinker@gmail.com>
2017-06-05 00:24:32 +0600

    Adds const qualifier

--- src/Event.cpp
+++ src/Event.cpp
@@ -4,6 +4,6 @@
 #include <BWAPI/Event.h>
 
 BwString* Event_getText(Event* self) {
-    const std::string* text = reinterpret_cast<std::string*>(self->text);
+    const std::string* const text = reinterpret_cast<std::string*>(self->text);
     return BwString_new(text->c_str(), static_cast<int>(text->length()));
 }
